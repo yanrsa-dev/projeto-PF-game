e881727f1fd35313bd3a551b60c39ac33db6dadc
Pedro Lucas <pedro_oliveiralucas@hotmail.com>
2025-04-02 14:50:43 -0300

    Adicionado o Dado
    Adicionando o dado para foco do jogo

---
+++ codigoF.js
@@ -0,0 +1,2 @@
+const dado = (numeros) => numeros[Math.floor(Math.random()* numeros.length)] // Uma função que pega uma lista com 6 numeros do dados e sorteia para retornar 1 valor
+const numerosDados = dado([1,2,3,4,5,6])
