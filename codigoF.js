8a4cd90d8c5d7e9ccf1ee6c0db89f9fdb11b0c3c
Pedro Lucas <pedro_oliveiralucas@hotmail.com>
2025-04-05 11:13:33 -0300

    Pagina de Quem Começa
    Códigos arrumados para decidir qual jogador ira começar

--- codigoF.js
+++ codigoF.js
@@ -1,9 +1,17 @@
 const dado = (numeros) => numeros[Math.floor(Math.random()* numeros.length)] // Uma função que pega uma lista com 6 numeros do dados e sorteia para retornar 1 valor
+
+
 const quemComeca = () => {
     const j1 = dado([1, 2, 3, 4, 5, 6])
     const j2 = dado([1, 2, 3, 4, 5, 6])
-    const resultado = j1>j2 ? alert("Jogador 1 irá começar"):j2>j1 ? alert('Jogador 2 irá começar'): 
-    document.getElementById('numero1').innerHTML = j1;
+    document.getElementById('numero1').innerHTML = j1; 
     document.getElementById('numero2').innerHTML = j2;
-    document.getElementById('resultado').innerHTML = resultado;
-}
+    setTimeout(() => {
+         const empate = (j1) => (j2) =>
+            j1 === j2 ? (alert("Empate! Role novamente"), quemComeca()): // Caso de empate ele emite o alerta e rerola os dados dos jogadores
+            (alert(j1 > j2 ? "Jogador 1 irá começar":"Jogador 2 irá começar"),
+            window.location.href = "jogo.html") // Redireciona para pagina do jogo
+            empate(j1)(j2)
+    },500)
+
+    } // Função para decidir quem começa
