a823e1fd1a86c2246b3e0ec605a669ecb2d45d13
Pedro Lucas <pedro_oliveiralucas@hotmail.com>
2025-04-05 16:06:21 -0300

    Mudanças Basicas no JS

--- codigoF.js
+++ codigoF.js
@@ -20,5 +20,3 @@ const quemComeca = () => {
     const primeiro = params.get("primeiro")
     document.getElementById("mensagem").innerText = 
     primeiro ? `O ${primeiro} começa o jogo!` : "Erro: Jogador não definido."
-},0) // Decidi armazenar a informação no URL como é uma informação simploria
-
