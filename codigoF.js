9dda53e8dc627e3758a8ec0092ac0c8bbb9bd7d0
yanrsa-dev <yanrochadesa10@gmail.com>
2025-04-08 13:58:27 -0300

    Update codigoF.js

--- codigoF.js
+++ codigoF.js
@@ -82,39 +82,13 @@ function prepararCelulasFuncional() {
 
   //JOGADOR 1
 
- const Jogador1Resultado=
-    
-    [J1Coluna1Linha1,J1Coluna1Linha2,J1Coluna1Linha3, // PRIMEIRA COLUNA=Jogador1Resultado[0][1][2]
-    J1Coluna2Linha1,J1Coluna2Linha2,J1Coluna2Linha3, // SEGUNDA COLUNA=Jogador1Resultado[3][4][5]
-    J1Coluna3Linha1,J1Coluna3Linha2,J1Coluna3Linha3] // TERCEIRA COLUNA=Jogador1Resultado[6][7][8]
 
- 
-
-const J1Coluna1= Jogador1Resultado.slice([0],[3])// função que separa a coluna 1
-const J1SomaColuna1= J1Coluna1.reduce((acc,x)=> acc+x)// função que soma e multiplica a coluna 1
-const J1Coluna2= Jogador1Resultado.slice([3],[6])// função que separa a coluna 2
-const J1SomaColuna2= (J1Coluna1.reduce((acc,x)=> acc+x))*2// função que soma e multiplica a coluna 2
-const J1Coluna3= Jogador1Resultado.slice([6])// função que separa a coluna 3
-const J1SomaColuna3= (J1Coluna1.reduce((acc,x)=> acc+x))*3// função que soma e multiplica a coluna 3
-const PontuaçãoJ1= J1SomaColuna1+J1SomaColuna2+J1SomaColuna3// função que soma as somas parciais das colunas 1,2 e 3 dando a pontuação final do jogador 1
 
 
   //JOGADOR 2
 
 
-  const Jogador2Resultado=
-    
-    [J2Coluna1Linha1,J2Coluna1Linha2,J2Coluna1Linha3, // PRIMEIRA COLUNA=Jogador2Resultado[0][1][2]
-    J2Coluna2Linha1,J2Coluna2Linha2,J2Coluna2Linha3, // SEGUNDA COLUNA=Jogador2Resultado[3][4][5]
-    J2Coluna3Linha1,J1Coluna3Linha2,J2Coluna3Linha3] // TERCEIRA COLUNA=Jogador2Resultado[6][7][8]
 
-const J2Coluna1= Jogador2Resultado.slice([0],[3])// função que separa a coluna 1
-const J2SomaColuna1= J2Coluna1.reduce((acc,x)=> acc+x)// função que soma e multiplica a coluna 1
-const J2Coluna2= Jogador2Resultado.slice([3],[6])// função que separa a coluna 2
-const J2SomaColuna2= (J2Coluna1.reduce((acc,x)=> acc+x))*2// função que soma e multiplica a coluna 2
-const J2Coluna3= Jogador2Resultado.slice([6])// função que separa a coluna 3
-const J2SomaColuna3= (J2Coluna1.reduce((acc,x)=> acc+x))*3// função que soma e multiplica a coluna 3
-const PontuaçãoJ2= J2SomaColuna1+J2SomaColuna2+J2SomaColuna3// função que soma as somas parciais das colunas 1,2 e 3 dando a pontuação final do jogador 2
 
 
 
